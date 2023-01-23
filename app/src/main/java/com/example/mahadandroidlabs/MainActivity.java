3f107d06416a30a5cb89de9da52de265312334a7
Mahad Mohamed <mahad8574@gmail.com>
2023-01-23 12:46:24 -0500

    Adding Edit/Button/Text Widget

--- app/src/main/java/com/example/mahadandroidlabs/MainActivity.java
+++ app/src/main/java/com/example/mahadandroidlabs/MainActivity.java
@@ -3,11 +3,17 @@ package com.example.mahadandroidlabs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        TextView mytext = findViewById(R.id.textview);
+        Button btn = findViewById(R.id.mybutton);
+        EditText myedit = findViewById(R.id.myedittext);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
