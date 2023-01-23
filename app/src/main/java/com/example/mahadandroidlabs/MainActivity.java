05a2ffe971031b3593ca6217b5e6b90e099ddeb3
Mahad Mohamed <mahad8574@gmail.com>
2023-01-23 13:24:19 -0500

    Attempt to get event button to work

--- app/src/main/java/com/example/mahadandroidlabs/MainActivity.java
+++ app/src/main/java/com/example/mahadandroidlabs/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.mahadandroidlabs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -12,8 +13,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         TextView mytext = findViewById(R.id.textview);
-        Button btn = findViewById(R.id.mybutton);
         EditText myedit = findViewById(R.id.myedittext);
+        final Button btn = findViewById(R.id.mybutton);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String editString = myedit.getText().toString();
+                mytext.setText( "Your edit text has: " + editString);
+            }
+        });
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
