b52cac4576af837e35f5d06e0ea6ce23346b1114
Mahad Mohamed <mahad8574@gmail.com>
2023-01-23 18:39:34 -0500

    Model

--- app/src/main/java/ui/MainActivity.java
+++ app/src/main/java/ui/MainActivity.java
@@ -1,6 +1,7 @@
-package com.example.mahadandroidlabs;
+package ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.view.View;
@@ -8,14 +9,21 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+
+import com.example.mahadandroidlabs.R;
+import com.example.mahadandroidlabs.data.MainViewModel;
 import com.example.mahadandroidlabs.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding variableBinding;
+    private MainViewModel model;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        model = new ViewModelProvider(this).get(MainViewModel.class);
 
         setContentView(variableBinding.getRoot());
         setContentView(R.layout.activity_main);
@@ -24,12 +32,13 @@ public class MainActivity extends AppCompatActivity {
         EditText myedit = variableBinding.myedittext;
         final Button btn = variableBinding.mybutton;
 
-        btn.setOnClickListener((View v) -> {
-            String editString = myedit.getText().toString();
-            mytext.setText("Your edit text has: " + editString);
+
+        variableBinding.textview.setText(model.editString);
+
+        variableBinding.mybutton.setOnClickListener(click -> {
+            model.editString.postValue(variableBinding.myedittext.getText().toString());
+            variableBinding.textview.setText("Your Edit Text has: " + model.editString);
         });
-
-
-
     }
 }
+
