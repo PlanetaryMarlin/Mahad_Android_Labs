5f06c709b171882dc689f852bb777f39228a3ba0
Mahad Mohamed <mahad8574@gmail.com>
2023-01-23 16:17:44 -0500

    Added View Binding

--- app/src/main/java/com/example/mahadandroidlabs/MainActivity.java
+++ app/src/main/java/com/example/mahadandroidlabs/MainActivity.java
@@ -8,16 +8,20 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.mahadandroidlabs.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding variableBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(variableBinding.getRoot());
         setContentView(R.layout.activity_main);
 
-        TextView mytext = findViewById(R.id.textview);
-        EditText myedit = findViewById(R.id.myedittext);
-        final Button btn = findViewById(R.id.mybutton);
+        TextView mytext = variableBinding.textview;
+        EditText myedit = variableBinding.myedittext;
+        final Button btn = variableBinding.mybutton;
 
         btn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
