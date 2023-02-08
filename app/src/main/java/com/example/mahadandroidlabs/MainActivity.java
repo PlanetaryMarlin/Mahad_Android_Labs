cbeaceb7c02b4f2005ab72e12660bdaa3379e963
Mahad Mohamed <mahad8574@gmail.com>
2023-02-08 15:26:44 -0500

    Lab 4 Step 2: Add Textview

--- app/src/main/java/com/example/mahadandroidlabs/MainActivity.java
+++ app/src/main/java/com/example/mahadandroidlabs/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.mahadandroidlabs;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +10,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.w( "MainActivity", "In onCreate() - Loading Widgets" );
+    }
+
+    @Override
+    protected void onDestroy() {
+
+        super.onDestroy();
+        Log.w( "MainActivity", "In onDestroy() - Loading Widgets" );
+    }
+
+    @Override
+    protected void onPause() {
+
+        super.onPause();
+        Log.w( "MainActivity", "In onPause() - Loading Widgets" );
+    }
+
+    @Override
+    protected void onResume() {
+
+        super.onResume();
+        Log.w( "MainActivity", "In onResume() - Loading Widgets" );
+    }
+
+    @Override
+    protected void onStop() {
+
+        super.onStop();
+        Log.w( "MainActivity", "In onStop() - Loading Widgets" );
+    }
+
+    @Override
+    protected void onStart() {
+
+        super.onStart();
+        Log.w( "MainActivity", "In onStart() - Loading Widgets" );
     }
 }
