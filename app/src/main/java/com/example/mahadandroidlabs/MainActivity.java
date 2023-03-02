552ae836947d9877925bbf08e23b2ed94c2f4e34
Mahad Mohamed <mahad8574@gmail.com>
2023-03-02 18:06:53 -0500

    Finish Test Cases and Javadoc

--- app/src/main/java/com/example/mahadandroidlabs/MainActivity.java
+++ app/src/main/java/com/example/mahadandroidlabs/MainActivity.java
@@ -3,12 +3,107 @@ package com.example.mahadandroidlabs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+
+/**
+ * This page allows user to type in a password, and check how complex the password is through various functions.
+ * @version 1.0
+ * @author Mahad Mohamed
+ */
 public class MainActivity extends AppCompatActivity {
+
+    /**   This holds the text at the center of the screen */
+    private TextView tv = null;
+    /** This hold the the edit box where the password is typed in*/
+    private EditText et = null;
+    /** This is the login button that confirms the editbox*/
+    private Button btn = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv = findViewById(R.id.textView);
+        EditText et = findViewById(R.id.editText);
+        Button btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(clk -> {
+            String password = et.getText().toString();
+
+            //checkPasswordComplexity(password);
+
+            if (checkPasswordComplexity(password) == true){
+                tv.setText("Your password meets the requirements");
+            }
+            else if (checkPasswordComplexity(password) == false) {
+                tv.setText("You Shall Not Pass!");
+            }
+        });
     }
-}
+
+    /**
+     * @param pw The String object that we are checking
+     * @return return true if password is complex enough.
+     */
+    boolean checkPasswordComplexity(String pw) {
+        boolean foundUpperCase, foundLowerCase, foundNumber, foundSpecial;
+        foundUpperCase = foundLowerCase = foundNumber = foundSpecial = false;
+        boolean complexEnough = true;
+
+
+        for (int i = 0; i <pw.length(); i++) {
+            char c = pw.charAt(i);
+
+            if (Character.isUpperCase(c))
+                foundUpperCase = true;
+            else if (Character.isLowerCase(c)) {
+                foundLowerCase = true;
+            }
+            else if (Character.isDigit(c)) {
+                foundNumber = true;
+            }
+
+            /**
+             * Checks if the character is not a number, letter, or empty spaces. If's not any of this, then it's considered special.
+             */
+            else if (!Character.isDigit(c)&& !Character.isLetter(c)&& !Character.isWhitespace(c)) {
+                foundSpecial = true;
+            }
+
+
+        }
+
+        if(!foundUpperCase) {
+            Toast.makeText(this,"Missing Uppercase letter",Toast.LENGTH_SHORT).show();  ;// Say that they are missing an upper case letter;
+            return false ;
+
+        }
+        else if( ! foundLowerCase) {
+            Toast.makeText(this,"Missing Lowercase letter",Toast.LENGTH_SHORT).show();  // Say that they are missing a lower case letter;
+            return false;
+        }
+
+        else if( ! foundNumber) {
+            Toast.makeText(this,"Missing Number",Toast.LENGTH_SHORT).show();  // Say that they are missing a lower case letter;
+            return false;
+        }
+
+        else if( ! foundSpecial) {
+            Toast.makeText(this,"Missing Special",Toast.LENGTH_SHORT).show();  // Say that they are missing a lower case letter;
+            return false;
+        }
+
+
+        return complexEnough;
+
+
+    }
+
+
+    }
