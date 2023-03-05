0e3d4cbef409dbe8b28e391ab32676bc43b0af12
Mahad Mohamed <mahad8574@gmail.com>
2023-03-05 16:01:56 -0500

    Finished Module 2. Text appear on screen like text messaging. Text saves when screen rotates

--- app/src/main/java/com/example/mahadandroidlabs/ChatRoom.java
+++ app/src/main/java/com/example/mahadandroidlabs/ChatRoom.java
@@ -2,47 +2,91 @@ package com.example.mahadandroidlabs;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.mahadandroidlabs.databinding.ActivityChatRoomBinding;
+import com.example.mahadandroidlabs.databinding.SentMessageBinding;
+
+import java.util.ArrayList;
 
 public class ChatRoom extends AppCompatActivity {
 
     ActivityChatRoomBinding binding;
+    ChatRoomViewModel chatModel ;
+    private ArrayList<String> messages;
+    private RecyclerView.Adapter myAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityChatRoomBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        chatModel = new ViewModelProvider(this).get(ChatRoomViewModel.class);
+        messages = chatModel.messages.getValue();
+            if(messages == null) {
+                chatModel.messages.postValue( messages = new ArrayList<String>());
+            }
+
+        binding.sendButton.setOnClickListener(click -> {
+            messages.add(binding.textInput.getText().toString());
+
+            //Noitfy when changes happens
+            myAdapter.notifyItemInserted(messages.size()-1);
+            myAdapter.notifyItemRemoved(messages.size()-1);
+
+
+            //clear previous text
+            binding.textInput.setText("");
+        });
+
+
+
 
         class MyRowHolder extends RecyclerView.ViewHolder {
+            TextView messageText;
+            TextView timeText;
             public MyRowHolder(@NonNull View itemView) {
                 super(itemView);
+                messageText = itemView.findViewById(R.id.messageText);
+                timeText = itemView.findViewById(R.id.timeText);
             }
         }
 
-        binding.recycleView.setAdapter(new RecyclerView.Adapter<MyRowHolder>() {
+        binding.recycleView.setAdapter(myAdapter=new RecyclerView.Adapter<MyRowHolder>() {
             @NonNull
             @Override
             public MyRowHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-                return null;
+                SentMessageBinding binding = SentMessageBinding.inflate(getLayoutInflater());
+                return new MyRowHolder(binding.getRoot());
             }
 
             @Override
             public void onBindViewHolder(@NonNull MyRowHolder holder, int position) {
+                holder.messageText.setText("");
+                holder.timeText.setText("");
+                String obj = messages.get(position);
+                holder.messageText.setText(obj);
 
             }
 
             @Override
             public int getItemCount() {
+                return messages.size();
+            }
+
+            public int getItemViewType(int position){
                 return 0;
             }
         });
+
+        binding.recycleView.setLayoutManager(new LinearLayoutManager(this));
 
 
     }
---
+++ app/src/main/java/com/example/mahadandroidlabs/ChatRoomViewModel.java
@@ -0,0 +1,10 @@
+package com.example.mahadandroidlabs;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.ArrayList;
+
+public class ChatRoomViewModel extends ViewModel {
+    public MutableLiveData<ArrayList<String>> messages = new MutableLiveData< >();
+}
